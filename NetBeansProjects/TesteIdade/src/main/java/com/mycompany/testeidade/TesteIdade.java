a3cf0c39e09db400076384cdafb379316209cea1
DGcoder-1 <rozieneazevedo1@gmail.com>
2026-04-06 10:01:48 -0300

    Add files via upload

---
+++ NetBeansProjects/TesteIdade/src/main/java/com/mycompany/testeidade/TesteIdade.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testeidade;
+
+/**
+ *
+ * @author 20251134010003
+ */
+public class TesteIdade {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
