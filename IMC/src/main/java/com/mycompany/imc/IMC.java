bd5bd1550c54f4febe1c2e9b0c9a52163080b40d
DGcoder-1 <rozieneazevedo1@gmail.com>
2026-04-07 10:06:44 -0300

    aula de poo dia 07/04

---
+++ IMC/src/main/java/com/mycompany/imc/IMC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.imc;
+
+/**
+ *
+ * @author 20251134010003
+ */
+public class IMC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
